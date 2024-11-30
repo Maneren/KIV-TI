def5a4d6f3d5dde46cbbc2b5853c7d4d143a9496
Maneren <maneren731@gmail.com>
2024-11-30 20:15:44 +0100

    linter + formatting

--- FSM.py
+++ FSM.py
@@ -6,17 +6,18 @@ class UnexpectedSymbol(Exception):
 
 
 class FSM:
-    '''
+    """
     Class for representing finite-state machines
-    '''
+    """
+
     # Dictionary where ids are keys and values are state objects
-    states: dict[str, State] = dict()
+    states: dict[str, State] = {}
 
     # Set of characters that represents alphabet of the finite-state machine
-    alphabet: set[str] 
+    alphabet: set[str]
 
     # Ids of initial state
-    initial: str  
+    initial: str
 
     # Ids of terminal states
     terminals: set[str]
@@ -29,26 +30,22 @@ class FSM:
 
     def generate_state_name(self) -> str:
         index: int = self.name_index
-        
+
         prefix_val: int = index // 26
-        suffix_val: int = index % 26        
+        suffix_val: int = index % 26
 
-        # Generating prefix
-        if prefix_val >= 1:
-            prefix: str = chr(ord("A") + prefix_val - 1)
-        else:
-            prefix = ""
+        prefix: str = chr(ord("A") + prefix_val - 1) if prefix_val >= 1 else ""
 
-        #Generating suffix
+        # Generating suffix
         suffix: str = chr(ord("A") + suffix_val)
 
-        #Incrementing name_index for next use
+        # Incrementing name_index for next use
         self.name_index += 1
 
         return prefix + suffix
 
     def add_state(self, id: str, name: str = "") -> None:
-        if name == "":
+        if not name:
             name = self.generate_state_name()
 
         new_state = State(name)
@@ -87,14 +84,15 @@ class FSM:
 
 
 class State:
-    '''
+    """
     Class for representing a state in finite-state machine
-    '''
+    """
+
     # Name of the state
     name: str
 
     # Dictionary where key is the character of the transition and value is the next state for given transition
-    nbs: dict[str, State] = dict() 
+    nbs: dict[str, State] = {}
 
     def __init__(self, name: str):
         self.name = name
@@ -104,7 +102,7 @@ class State:
 
 
 if __name__ == "__main__":
-    alphabet: set = {"a","b"}
+    alphabet: set = {"a", "b"}
     fsm: FSM = FSM(alphabet)
 
     fsm.add_transition("1", "2", "b")
